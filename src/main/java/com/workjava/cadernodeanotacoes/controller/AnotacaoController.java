5d8477092c4873de9a0c7e8d7cdb0d01a144c7b1
Eriton Santos <eritonsg@gmail.com>
2019-05-14 22:13:07 -0300

    Adicionando edição de anotação.

--- src/main/java/com/workjava/cadernodeanotacoes/controller/AnotacaoController.java
+++ src/main/java/com/workjava/cadernodeanotacoes/controller/AnotacaoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,6 +39,28 @@ public class AnotacaoController {
 		anotacaoService.salvar(anotacao);
 		
 		redirectAttributes.addFlashAttribute("mensagem", "Anotação criada com sucesso!");
+		
+		return new ModelAndView("redirect:/anotacoes/nova");
+	}
+	
+	@GetMapping("/{id}/editar")
+	public ModelAndView editar(@PathVariable Long id) {
+		ModelAndView mv = new ModelAndView("anotacoes/anotacoes-cadastro");
+		mv.addObject("anotacao", anotacaoService.buscarPorID(id));
+		return mv;
+	}
+	
+	@PostMapping("/{id}/editar")
+	public ModelAndView atualizar(@PathVariable Long id,
+			@Valid Anotacao anotacao, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			return nova(anotacao);
+		}
+		
+		anotacaoService.atualizar(id, anotacao);
+		
+		redirectAttributes.addFlashAttribute("mensagem", "Anotação atualizada com sucesso!");
 		
 		return new ModelAndView("redirect:/anotacoes/nova");
 	}
--- src/main/java/com/workjava/cadernodeanotacoes/service/AnotacaoService.java
+++ src/main/java/com/workjava/cadernodeanotacoes/service/AnotacaoService.java
@@ -1,7 +1,11 @@
 package com.workjava.cadernodeanotacoes.service;
 
+import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +24,16 @@ public class AnotacaoService {
 	
 	public List<Anotacao> buscarTodos() {
 		return repo.findAll();
+	}
+
+	public Anotacao buscarPorID(Long id) {
+		return repo.getOne(id);
+	}
+
+	public void atualizar(Long id, Anotacao anotacao) {
+		Anotacao anotacaoSalva = repo.getOne(id);
+		BeanUtils.copyProperties(anotacao, anotacaoSalva, "id", "cadastro");
+		repo.save(anotacaoSalva);
 	}
 
 }
